296b6f75de74a7f375bcbbfb295333afa6575d8e
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2026-01-16 21:26:34 +0530

    Add login system with user validation and templates
    Implemented a basic login system in app.py with hardcoded user validation. Added login, demo, and about HTML templates to support user authentication and navigation.

--- Day-02/app.py
+++ Day-02/app.py
@@ -1,13 +1,28 @@
-from flask import Flask,request
+from flask import Flask,render_template, request
+
+valid_users = {
+    "prasxor": "123",
+    "rahul": "456",
+    "swetha": "789"
+}
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    if response
-    return "this is home page"
+@app.route("/")
+def login():
+    return render_template("login.html")
 
-@app.route("/about")
-def about():
-    return "this is about section"
-
+@app.route("/sumbit", methods=["POST"])
+def submit():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    # return render_template("login.html")
+    # if username == "prasxor" and password == "123":
+    #     return render_template("demo.html", name = username)
+    # else:
+    #     return "invalid credentials"
+    
+    if username in valid_users and password == valid_users[username]:
+        return render_template("demo.html", name = username)
+    else:
+        return "invalid credentials"
