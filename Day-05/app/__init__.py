f2a382c550dd182b5171d97e71a5659412bc36b8
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2026-02-14 10:23:24 +0530

    feat : added file structure for flask

---
+++ Day-05/app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from routes.auth import auth_bp
+
+def create_app():
+    app = Flask(__name__)
+
+    app.secret_key = "my-secret-key"
+
+    app.register_blueprint(auth_bp)
+
+    return app
